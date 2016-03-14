75c6fe5d78304c55846ba1f93395c201d3a6fd13
j.taneichi <j.tane3ma@gmail.com>
2016-03-15 01:10:24 +0900

    change haml

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
 
   resources :books do
-    resources :pages
+    resources :pages do
+      resources :images, only: [] do
+        member do
+          get "thubmnail"
+        end
+      end
+    end
   end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
