0691f79eadf7e984f1fbe84d75fc8c5f84038aed
j.taneichi <j.tane3ma@gmail.com>
2016-03-16 20:28:15 +0900

    add association

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,5 @@
 class Image < ActiveRecord::Base
   belongs_to :page
+  has_many :orders
+  has_many :pages, through: :orders
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -3,6 +3,7 @@ require "uri"
 class Page < ActiveRecord::Base
   belongs_to :book
   has_many :images
-
+  has_many :orders
+  has_many :images, through: :orders
 end
 
