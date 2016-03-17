d290add2f5f80fcdf3312c8beaa776b1f0e5c217
j.taneichi <j.tane3ma@gmail.com>
2016-03-17 20:55:06 +0900

    add dashboards

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,9 @@ Rails.application.routes.draw do
     end
   end
 
+  get 'dashboard' => 'dashboards#index'
+
+  root 'dashboards#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
